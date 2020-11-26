81b09b41e2e92f2fe9c271025541e5348115b41c
Ewan Watt <watt.ewan99@gmail.com>
2020-11-25 23:41:42 -0800

    fixing package.json

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = mongoKey
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+//var dev_db_url = mongoKey
+var mongoDB = process.env.MONGODB_URI //|| dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
