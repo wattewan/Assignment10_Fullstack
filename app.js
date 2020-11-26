7d6405c95f0dd1bdeff9948bcf40f0f79c51f1ec
Ewan Watt <watt.ewan99@gmail.com>
2020-11-25 23:41:31 -0800

    fixing package.json

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var mongoKey = require('./key.js')
 
 var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Admin:P@ssw0rd@cluster0.m4tyv.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = mongoKey
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
---
+++ key.js
@@ -0,0 +1,3 @@
+const dev_db_url = 'mongodb+srv://Admin:P@ssw0rd@cluster0.m4tyv.mongodb.net/local_library?retryWrites=true&w=majority'
+
+module.exports = dev_db_url;
